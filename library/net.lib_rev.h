1b5a64e952e7a953da30cedca920bd2b59dae700
John-Mark Bell <jmb@netsurf-browser.org>
2025-12-14 20:39:45 +0000

    Upstream clib2 1_214

---
+++ library/net.lib_rev.h
@@ -0,0 +1,6 @@
+#define VERSION		1
+#define REVISION	214
+#define DATE		"27.4.2017"
+#define VERS		"net.lib 1.214"
+#define VSTRING		"net.lib 1.214 (27.4.2017)\r\n"
+#define VERSTAG		"\0$VER: net.lib 1.214 (27.4.2017)"
